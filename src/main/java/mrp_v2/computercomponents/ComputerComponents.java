cd96d470d5b17bd53ffb6bbcac07713f1a237497
mrp-v2 <matthrewp@gmail.com>
2020-09-04 10:31:34 -0400

    created mod

---
+++ src/main/java/mrp_v2/computercomponents/ComputerComponents.java
@@ -0,0 +1,8 @@
+package mrp_v2.computercomponents;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ComputerComponents.ID) public class ComputerComponents
+{
+    public static final String ID = "computercomponents";
+}
