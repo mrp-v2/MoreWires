504c79b7a736c45d6f5520b4b96578bd7c513e8a
mrp-v2 <matthrewp@gmail.com>
2021-01-02 13:59:54 -0500

    finished fixing #1, hopefully didn't break anything
    still has superior performance to redstone, yay

---
+++ src/main/java/mrp_v2/morewires/block/util/InfiniwireChainParent.java
@@ -0,0 +1,22 @@
+package mrp_v2.morewires.block.util;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InfiniwireChainParent
+{
+    final Map<BlockPos, InfiniwireChain> chainMap = new HashMap<>();
+
+    public void update(BlockPos updateOrigin, World world)
+    {
+        chainMap.get(updateOrigin).update(world);
+    }
+
+    public boolean containsPos(BlockPos pos)
+    {
+        return chainMap.containsKey(pos);
+    }
+}
---
+++ src/main/java/mrp_v2/morewires/block/util/InfiniwireGraphBuilder.java
@@ -0,0 +1,283 @@
+package mrp_v2.morewires.block.util;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.*;
+
+public class InfiniwireGraphBuilder
+{
+    protected final Map<BlockPos, BlockState> blockMap = new HashMap<>();
+    protected final Map<BlockPos, List<Connection>> connectionMap = new HashMap<>();
+    protected final Set<Connection> connectionSet = new HashSet<>();
+    protected final BlockPos originPos;
+    protected final BlockState originState;
+
+    public InfiniwireGraphBuilder(BlockPos originPos, BlockState originState)
+    {
+        this.originPos = originPos;
+        this.originState = originState;
+    }
+
+    protected List<Connection> getOrCreateConnectionList(BlockPos pos)
+    {
+        if (connectionMap.containsKey(pos))
+        {
+            return connectionMap.get(pos);
+        } else
+        {
+            ArrayList<Connection> list = new ArrayList<>();
+            connectionMap.put(pos, list);
+            return list;
+        }
+    }
+
+    public InfiniwireChainParent build()
+    {
+        Set<InfiniwireChain> chains = new HashSet<>();
+        for (Connection connection : connectionSet)
+        {
+            InfiniwireChain a = null, b = null;
+            for (InfiniwireChain chain : chains)
+            {
+                if (chain.positions.containsKey(connection.a))
+                {
+                    a = chain;
+                }
+                if (chain.positions.containsKey(connection.b))
+                {
+                    b = chain;
+                }
+            }
+            if (a != null && a == b)
+            {
+                continue;
+            }
+            if (a != null && b != null)
+            {
+                setupChainRelationship(a, b, connection.connectionType, chains);
+            } else if (connection.connectionType == ConnectionType.BIDIRECTIONAL)
+            {
+                if (a == null && b != null)
+                {
+                    b.positions.put(connection.a, blockMap.get(connection.a));
+                } else if (a != null)
+                {
+                    a.positions.put(connection.b, blockMap.get(connection.b));
+                } else
+                {
+                    a = new InfiniwireChain();
+                    a.positions.put(connection.a, blockMap.get(connection.a));
+                    a.positions.put(connection.b, blockMap.get(connection.b));
+                    chains.add(a);
+                }
+            } else
+            {
+                if (a == null && b != null)
+                {
+                    a = new InfiniwireChain();
+                    a.positions.put(connection.a, blockMap.get(connection.a));
+                    chains.add(a);
+                    setupChainRelationship(a, b, connection.connectionType, chains);
+                } else if (a != null)
+                {
+                    b = new InfiniwireChain();
+                    b.positions.put(connection.b, blockMap.get(connection.b));
+                    chains.add(b);
+                    setupChainRelationship(a, b, connection.connectionType, chains);
+                } else
+                {
+                    a = new InfiniwireChain();
+                    a.positions.put(connection.a, blockMap.get(connection.a));
+                    chains.add(a);
+                    b = new InfiniwireChain();
+                    b.positions.put(connection.b, blockMap.get(connection.b));
+                    chains.add(b);
+                    setupChainRelationship(a, b, connection.connectionType, chains);
+                }
+            }
+        }
+        InfiniwireChainParent parent = new InfiniwireChainParent();
+        for (InfiniwireChain chain : chains)
+        {
+            for (BlockPos pos : chain.positions.keySet())
+            {
+                parent.chainMap.put(pos, chain);
+            }
+        }
+        if (chains.size() == 0)
+        {
+            InfiniwireChain chain = new InfiniwireChain();
+            chain.positions.put(originPos, originState);
+            parent.chainMap.put(originPos, chain);
+        }
+        return parent;
+    }
+
+    protected void setupChainRelationship(InfiniwireChain a, InfiniwireChain b, ConnectionType connectionType,
+            Set<InfiniwireChain> chains)
+    {
+        switch (connectionType)
+        {
+            case BIDIRECTIONAL:
+                checkForRecursivePowering(mergeChains(a, b, chains), chains);
+                break;
+            case A_TO_B:
+                a.chainsPowering.add(b);
+                b.chainsPoweredBy.add(a);
+                checkForRecursivePowering(a, chains);
+                break;
+            case B_TO_A:
+                b.chainsPowering.add(a);
+                a.chainsPoweredBy.add(b);
+                checkForRecursivePowering(a, chains);
+                break;
+        }
+    }
+
+    protected InfiniwireChain mergeChains(InfiniwireChain a, InfiniwireChain b, Set<InfiniwireChain> chains)
+    {
+        InfiniwireChain combinedChain = new InfiniwireChain();
+        combinedChain.positions.putAll(a.positions);
+        combinedChain.positions.putAll(b.positions);
+        a.chainsPoweredBy.remove(b);
+        b.chainsPoweredBy.remove(a);
+        a.chainsPowering.remove(b);
+        b.chainsPowering.remove(a);
+        combinedChain.chainsPoweredBy.addAll(a.chainsPoweredBy);
+        for (InfiniwireChain chain : a.chainsPoweredBy)
+        {
+            chain.chainsPowering.remove(a);
+            chain.chainsPowering.add(combinedChain);
+        }
+        combinedChain.chainsPoweredBy.addAll(b.chainsPoweredBy);
+        for (InfiniwireChain chain : b.chainsPoweredBy)
+        {
+            chain.chainsPowering.remove(b);
+            chain.chainsPowering.add(combinedChain);
+        }
+        combinedChain.chainsPowering.addAll(a.chainsPowering);
+        for (InfiniwireChain chain : a.chainsPowering)
+        {
+            chain.chainsPoweredBy.remove(a);
+            chain.chainsPoweredBy.add(combinedChain);
+        }
+        combinedChain.chainsPowering.addAll(b.chainsPowering);
+        for (InfiniwireChain chain : b.chainsPowering)
+        {
+            chain.chainsPoweredBy.remove(b);
+            chain.chainsPoweredBy.add(combinedChain);
+        }
+        chains.remove(a);
+        chains.remove(b);
+        chains.add(combinedChain);
+        return combinedChain;
+    }
+
+    protected void checkForRecursivePowering(InfiniwireChain chain, Set<InfiniwireChain> chains)
+    {
+        checkForRecursivePowering(chain, chain, chains, new HashSet<>(), new Stack<>());
+    }
+
+    protected void mergeChains(Set<InfiniwireChain> chainsToMerge, Set<InfiniwireChain> chains)
+    {
+        if (chainsToMerge.size() < 2)
+        {
+            return;
+        }
+        InfiniwireChain[] chainArray = chainsToMerge.toArray(new InfiniwireChain[0]);
+        if (chainsToMerge.size() == 2)
+        {
+            checkForRecursivePowering(mergeChains(chainArray[0], chainArray[1], chains), chains);
+        }
+        InfiniwireChain combined = chainArray[0];
+        for (int i = 1; i < chainArray.length; i++)
+        {
+            combined = mergeChains(combined, chainArray[i], chains);
+        }
+        checkForRecursivePowering(combined, chains);
+    }
+
+    protected void checkForRecursivePowering(InfiniwireChain current, InfiniwireChain start,
+            Set<InfiniwireChain> chains, Set<InfiniwireChain> checkedChains, Stack<InfiniwireChain> currentPath)
+    {
+        checkedChains.add(current);
+        for (InfiniwireChain chain : current.chainsPowering)
+        {
+            currentPath.push(chain);
+            if (chain == start)
+            {
+                mergeChains(new HashSet<>(currentPath), chains);
+                return;
+            }
+            if (!checkedChains.contains(chain))
+            {
+                checkForRecursivePowering(chain, start, chains, checkedChains, currentPath);
+            }
+            currentPath.pop();
+        }
+    }
+
+    public void addNewConnection(BlockPos aPos, BlockState aState, BlockPos bPos, BlockState bState,
+            ConnectionType connectionType)
+    {
+        Connection connection = new Connection(aPos, bPos, connectionType);
+        if (!connectionSet.contains(connection))
+        {
+            connection.install();
+            if (!blockMap.containsKey(aPos))
+            {
+                blockMap.put(aPos, aState);
+            }
+            if (!blockMap.containsKey(bPos))
+            {
+                blockMap.put(bPos, bState);
+            }
+        }
+    }
+
+    public enum ConnectionType
+    {
+        BIDIRECTIONAL, A_TO_B, B_TO_A
+    }
+
+    protected class Connection
+    {
+        protected final BlockPos a, b;
+        protected final ConnectionType connectionType;
+
+        protected Connection(BlockPos a, BlockPos b, ConnectionType connectionType)
+        {
+            this.a = a;
+            this.b = b;
+            this.connectionType = connectionType;
+        }
+
+        protected void install()
+        {
+            getOrCreateConnectionList(a).add(this);
+            getOrCreateConnectionList(b).add(this);
+            connectionSet.add(this);
+        }
+
+        @Override public int hashCode()
+        {
+            return (a.hashCode() + b.hashCode());
+        }
+
+        @Override public boolean equals(Object o)
+        {
+            if (this == o)
+            {
+                return true;
+            }
+            if (!(o instanceof Connection))
+            {
+                return false;
+            }
+            Connection other = (Connection) o;
+            boolean equalsPositions = a.equals(other.a) && b.equals(other.b) || a.equals(other.b) && b.equals(other.a);
+            return equalsPositions && connectionType == other.connectionType;
+        }
+    }
+}
